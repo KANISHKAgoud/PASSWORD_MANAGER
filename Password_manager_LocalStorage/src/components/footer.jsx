4b5600b82ced10499a5e7c1c72abf8816ddf5edb
KANISHKA <goudkanishka@gmail.com>
2025-02-05 15:13:27 +0530

    responsive

---
+++ Password_manager_LocalStorage/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='mb-0 pb-0 overflow-auto relative'>
+      <div className='bg-black max-w-full text-center h-[8vh] text-white md:flex md:justify-around md:items-center'>
+        <div className='text-4xl font-extrabold'>
+          LockVault
+        </div>
+        <div>Created with <i class="fa-solid fa-heart"></i> by Kanishka</div>
+      </div>
+    </div>
+  )
+}
+
+export default footer
